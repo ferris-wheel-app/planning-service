6fb17c23ece782469c8e6f78e8661bedcaeb4eb7
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-05-25 23:19:12 +0100

    Implementing the life-scheduler and the laser-donut ordering.

--- src/main/scala/com/ferris/planning/scheduler/LifeScheduler.scala
+++ src/main/scala/com/ferris/planning/scheduler/LifeScheduler.scala
@@ -1,17 +1,36 @@
-//package com.ferris.planning.scheduler
-//
-//import com.ferris.planning.model.Model._
-//
-//trait LifeScheduler {
-//
-//  def schedule(laserDonuts: Seq[ScheduledLaserDonut]): Seq[ScheduledLaserDonut] = {
-//    val topTier = laserDonuts.filter(_.tier == 1)
-//
-//  }
-//}
-//
-//object LaserDonutOrdering extends Ordering[ScheduledLaserDonut] {
-//  override def compare(x: ScheduledLaserDonut, y: ScheduledLaserDonut) = {
-//
-//  }
-//}
+package com.ferris.planning.scheduler
+
+import scala.util.Random
+
+import com.ferris.planning.model.Model._
+
+trait LifeScheduler {
+
+  def schedule(laserDonuts: Seq[ScheduledLaserDonut]): Seq[ScheduledLaserDonut] = {
+    val topTier = laserDonuts.filter(_.tier == 1)
+    val incompleteItems = incomplete(laserDonuts)
+    val nextLaserDonut = planned(incompleteItems) match {
+      case Nil => {}
+      case notStartedItems => chooseRandom(notStartedItems)
+    }
+  }
+
+  private def incomplete(laserDonuts: Seq[ScheduledLaserDonut]): Seq[ScheduledLaserDonut] = {
+    laserDonuts.filter(_.status != Statuses.Complete)
+  }
+
+  private def planned(laserDonuts: Seq[ScheduledLaserDonut]): Seq[ScheduledLaserDonut] = {
+    laserDonuts.filter(_.status == Statuses.Planned)
+  }
+
+  def chooseRandom[T](list: Seq[T]): Option[T] = {
+    if (list.nonEmpty) Some(list(Random.nextInt(list.length)))
+    else None
+  }
+}
+
+object LaserDonutOrdering extends Ordering[ScheduledLaserDonut] {
+  override def compare(x: ScheduledLaserDonut, y: ScheduledLaserDonut) = {
+
+  }
+}
