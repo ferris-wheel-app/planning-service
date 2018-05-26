81a79e6887515d4d3458f0de56743c88a4791fd6
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-05-26 13:46:22 +0100

    Implementing the life-conveyor.

--- src/main/scala/com/ferris/planning/scheduler/LifeConveyor.scala
+++ src/main/scala/com/ferris/planning/scheduler/LifeConveyor.scala
@@ -7,15 +7,17 @@ import com.ferris.planning.model.Model._
 
 trait LifeConveyor {
 
-  def refresh(laserDonuts: Seq[ScheduledLaserDonut], lastUpdate: LocalDateTime): Seq[ScheduledLaserDonut] = {
+  def refresh(laserDonuts: Seq[ScheduledLaserDonut]): Seq[ScheduledLaserDonut] = {
     val (completed, leftOvers) = laserDonuts.partition(_.status == Statuses.Complete)
     val (planned, inProgress) = leftOvers.partition(_.status == Statuses.Planned)
     val
   }
 
   private def sortByFurthest(laserDonuts: Seq[ScheduledLaserDonut]): Seq[ScheduledLaserDonut] = {
-
+    laserDonuts.sortBy(laserDonut => LocalDateTime.now.minus(laserDonut.lastPerformed.head))
   }
+
+  private def sortBy
 
   private def shift(laserDonuts: Seq[ScheduledLaserDonut], steps: Int): Seq[ScheduledLaserDonut] = {
     ???
