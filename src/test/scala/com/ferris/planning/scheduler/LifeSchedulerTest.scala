a6a4196c6913c2d6f96c3f137ee317b7df3a86cf
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-06-10 18:41:23 +0100

    Testing the life-scheduler.

--- src/test/scala/com/ferris/planning/scheduler/LifeSchedulerTest.scala
+++ src/test/scala/com/ferris/planning/scheduler/LifeSchedulerTest.scala
@@ -4,6 +4,7 @@ import java.time.LocalDateTime
 
 import com.ferris.planning.config.PlanningServiceConfig
 import org.scalatest.{FunSpec, Matchers}
+import com.ferris.planning.model.Model.Statuses._
 import com.ferris.planning.sample.SampleData.domain._
 import com.ferris.planning.utils.MockTimerComponent
 import com.ferris.planning.utils.PlanningImplicits._
@@ -20,24 +21,59 @@ class LifeSchedulerTest extends FunSpec with Matchers {
 
   describe("a life-scheduler") {
     it("should leave a pyramid unchanged, if a week has not passed since it was last updated") {
+      val context = newContext()
       val previousUpdate = LocalDateTime.now
       val nextUpdate = previousUpdate.plusDays(3)
       val pyramid = scheduledPyramid.copy(
         laserDonuts = scheduledLaserDonut :: scheduledLaserDonut :: Nil,
         lastUpdate = Some(previousUpdate)
       )
-      val context = newContext()
       when(context.timer.now).thenReturn(nextUpdate.toLong)
 
       context.lifeScheduler.refreshPyramid(pyramid) shouldBe pyramid
     }
 
     it("should leave a pyramid unchanged, if there are no scheduled laser-donuts") {
+      val context = newContext()
+      val pyramid = scheduledPyramid.copy(
+        laserDonuts = Nil,
+        lastUpdate = None
+      )
 
+      context.lifeScheduler.refreshPyramid(pyramid) shouldBe pyramid
     }
 
     it("should perform a shift, when there are completed laser-donuts") {
+      val context = newContext()
+      val previousUpdate = LocalDateTime.now
+      val nextUpdate = previousUpdate.plusDays(7)
 
+      val d = scheduledLaserDonut
+      val tier1 = d.copy(id = 1, tier = 1, status = Planned) :: d.copy(id = 2, tier = 1, status = Complete) :: d.copy(id = 3, tier = 1, status = Complete) :: Nil
+      val tier2 = d.copy(id = 4, tier = 2, status = Planned) :: d.copy(id = 5, tier = 2, status = Planned) :: d.copy(id = 6, tier = 2, status = Planned) :: Nil
+      val tier3 = d.copy(id = 7, tier = 3, status = Planned) :: d.copy(id = 8, tier = 3, status = Planned) :: d.copy(id = 9, tier = 3, status = Planned) :: Nil
+      val originalPyramid = scheduledPyramid.copy(
+        laserDonuts = tier1 ++ tier2 ++ tier3,
+        lastUpdate = Some(previousUpdate)
+      )
+      when(context.timer.now).thenReturn(nextUpdate.toLong)
+
+      val updatedPyramid = context.lifeScheduler.refreshPyramid(originalPyramid)
+      val shiftedDonuts = updatedPyramid.laserDonuts
+      val newTopTier = shiftedDonuts.filter(_.tier == 1)
+      val newTier2 = shiftedDonuts.filter(_.tier == 2)
+      val newTier3 = shiftedDonuts.filter(_.tier == 3)
+
+      shiftedDonuts.size shouldBe originalPyramid.laserDonuts.size - 2
+
+      newTopTier.size shouldBe 3
+      newTopTier should contain (tier1.head)
+      newTopTier.count(donut => tier2.map(_.copy(tier = 1)).contains(donut)) shouldBe 2
+
+      newTier2.size shouldBe 3
+      newTier2.count(donut => tier3.map(_.copy(tier = 2)).contains(donut)) shouldBe 2
+
+      newTier3.size shouldBe 1
     }
 
     it("should perform a shift, when there are no completed laser-donuts, but there has been acceptable progress") {
